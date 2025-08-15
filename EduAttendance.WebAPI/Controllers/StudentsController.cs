afa5bc90dbbe4556c709583f12f998dea56838f3
MertAlii <Aslancocukmz@gmail.com>
2025-08-15 16:24:21 +0300

    Lesson 1

---
+++ EduAttendance.WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using EduAttendance.WebAPI.Context;
+using EduAttendance.WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduAttendance.WebAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public sealed class StudentsController(ApplicationDbContext dbContext) : ControllerBase
+{
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        await Task.Delay(5000, cancellationToken);
+        var students = await dbContext.Students.ToListAsync();
+        return Ok(students);
+    }
+}
--- EduAttendance.WebAPI/Program.cs
+++ EduAttendance.WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using EduAttendance.WebAPI.Context;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +8,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(opt =>
 {
     opt.UseSqlServer("Data Source=MERTALII\\SQLEXPRESS;database=EduAttendanceDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 });
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
+app.MapOpenApi();
+app.MapScalarApiReference();
 
+app.MapControllers();
 
 app.Run();
